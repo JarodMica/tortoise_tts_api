0ce0b71c625601b842e195608a157ebedd73f06e
Jarod Mica <jarodmica@gmail.com>
2024-09-17 14:20:50 -0700

    make update

--- test.py
+++ test.py
@@ -1,33 +1,21 @@
-from tortoise_tts_api.infer import *
+from tortoise_tts_api.inference.load import load_tts
+from tortoise_tts_api.inference.generate import generate
+import time
 
-
-
-# result = tortoise_inference(text="Hello, world! This is a test of the Tortoise TTS system.",
-#     voice="melina",  # Replace with the name of your voice profile
-#     seed=42,  # Optional: for reproducibility
-#     temperature=0.8,  # Optional: adjust as needed
-#     top_p=0.9,  # Optional: adjust as needed
-#     num_autoregressive_samples=16,  # Optional
-#     diffusion_iterations=100,  # Optional
-#     diffusion_temperature=1.0,  # Controls variance for diffusion model
-#     cvvp_weight=0.0,  # Optional weight for CVVP model in selecting the best output
-#     experimentals=[],  # Optional: e.g., ["Half Precision", "Conditioning-Free"]
-#     autoregressive_model_path=None,  # Optional: specify if using a custom model
-#     diffusion_model=None,  # Optional: specify if using a custom diffusion model
-#     tokenizer_json=None,  # Optional: specify a custom tokenizer JSON file
-#     use_deepspeed=True,  # Optional: enable DeepSpeed for optimized inference
-#     candidates=1,  # Number of speech samples to generate
-#     length_penalty=1.0,  # Penalizes the model for generating overly long sentences
-#     repetition_penalty=2.0,  # Penalizes the model for repeating itself
-#     cond_free_k=1.0,  # Parameter for conditioning-free diffusion
-#     diffusion_sampler="P",  # Sampler for diffusion model
-#     breathing_room=8,  # Allows some breathing space between tokens
-#     emotion=None  # Emotion to inject into the speech synthesis
-    # )
+tts = load_tts(autoregressive_model_path="1204_gpt.pth",
+               diffusion_model_path=None,
+               vocoder_name=None,
+               tokenizer_json_path="ja_base256_tokenizer.json",
+               use_deepspeed=True,
+               use_hifigan=False)
 
 while True:
-    result = tortoise_inference()
-    print(f"The audio path is: {result}")
-    input()
+    start_time = time.time()
+    result = generate(tts=tts, text="これはなんですか", use_hifigan=False)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
 
-    
+    print(f"The audio path is: {result}")
+    print(f"Time taken: {elapsed_time:.2f} seconds")
+
+    input("Press Enter to generate again...")
